dd060f54c7c1b7726cd94fbee1a1f1685ce04896
Conor Moran <G00244200@gmit.ie>
2012-12-08 20:57:37 +0000

    save file working

--- src/v2/MoodleGIFTQuiz.java
+++ src/v2/MoodleGIFTQuiz.java
@@ -4,8 +4,10 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -13,6 +15,11 @@ import javax.swing.ScrollPaneConstants;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import javax.swing.Action;
 
 public class MoodleGIFTQuiz extends JFrame {
@@ -63,6 +70,38 @@ public class MoodleGIFTQuiz extends JFrame {
 		getContentPane().add(btnAddQuestion, "flowx,cell 0 2");
 		
 		JButton btnNewButton = new JButton("Save GIFT File");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				saveBActionPerformed();
+				
+				
+//				//Opens filechooser
+//				JFileChooser fc = new JFileChooser(JFileChooser.FILE_FILTER_CHANGED_PROPERTY); 
+//				fc.setDialogTitle("Save .txt File");
+//				
+//				if (fc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION)
+//				{
+//					//If they clicked yes call fileSaver method
+//					fileSaver(fc);
+//				}
+//				else
+//				{
+//					//Show cancelled message
+//					JOptionPane.showMessageDialog(this, "Save Cancelled.", "Word Replacer Application Save Cancelled", JOptionPane.WARNING_MESSAGE);
+//				}
+				
+//				try	{
+//					//String s = textArea.getText();
+//					File f = new File("Quiz.gift");
+//					FileWriter fw = new FileWriter(f);
+//					fw.write(s);
+//				} catch(IOException ioe) {
+//					System.out.println("Exception Caught : " +ioe.getMessage());
+//				}
+			}
+		});
 		getContentPane().add(btnNewButton, "flowx,cell 1 2,alignx right");
 	}
 	
@@ -80,4 +119,65 @@ public class MoodleGIFTQuiz extends JFrame {
 		public void actionPerformed(ActionEvent e) {
 		}
 	}
+	
+	private void saveBActionPerformed() //java.awt.event.ActionEvent evt
+	{ 
+		//Opens filechooser
+		JFileChooser fc = new JFileChooser(JFileChooser.FILE_FILTER_CHANGED_PROPERTY); 
+		fc.setDialogTitle("Save .gift File");
+		
+		if (fc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION)
+		{
+			//If they clicked yes call fileSaver method
+			fileSaver(fc);
+		}
+		else
+		{
+			//Show cancelled message
+			JOptionPane.showMessageDialog(this, "Save Cancelled.", "Word Replacer Application Save Cancelled", JOptionPane.WARNING_MESSAGE);
+		}
+	}
+	//End saveBActionPerformed
+	
+	private void fileSaver(JFileChooser fc)
+	//Saves string to file, pass in FileChooser
+	{
+		File file = fc.getSelectedFile();
+		String textToSave = textArea.getText();
+		BufferedWriter writer = null;
+
+		//Check for legal file extension (.gift)	
+		String fileExtension = file.getPath();
+		
+		//Set extension to .gift if not already	
+		if(!fileExtension.toLowerCase().endsWith(".gift"))
+		{
+			file = new File(fileExtension + ".gift");
+		}
+		
+		try
+		{
+			writer = new BufferedWriter( new FileWriter(file));
+			writer.write(textToSave); //.replaceAll("\n", System.getProperty("line.seperator")));
+			
+			JOptionPane.showMessageDialog(this, "Message saved. (" + file.getName() + ")", "Page Saved Successfully", JOptionPane.INFORMATION_MESSAGE);
+		}
+		catch (IOException e)
+		{ }
+
+		//Close writer
+		finally
+		{
+			try
+			{
+				if(writer != null)
+				{
+					writer.close();
+				}
+			}
+			catch (IOException e)
+			{e.printStackTrace(); }
+		}
+	}
+	//End fileSaver
 }
